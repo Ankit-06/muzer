eac1a864738f1e91cd5f7e7221b052dd22c71ff0
Ankit Mishra <ankit@Ankits-MacBook-Air.local>
2025-07-04 01:18:22 +0530

    added youtube api for video details

--- app/api/streams/route.ts
+++ app/api/streams/route.ts
@@ -1,7 +1,11 @@
 import { prismaClient } from "@/app/lib/db";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
-const youTube_REGEX = new RegExp("^https://www.youtube.com/watch?v=[w-]{11}$");
+// @ts-ignore
+import youtubesearchapi from "youtube-search-api";
+
+const youTube_REGEX =
+  /^(https?:\/\/)?(www\.)?(youtube\.com\/(watch\?v=|embed\/|shorts\/)|youtu\.be\/)[\w\-]{11}(&\S*)?$/;
 
 const CreateStreamSchema = z.object({
   creatorId: z.string(),
@@ -26,13 +30,32 @@ export async function POST(req: NextRequest) {
 
     const extractedId = data.url.split("?v=")[1];
 
-    await prismaClient.stream.create({
+    const videoDetails = await youtubesearchapi.GetVideoDetails(extractedId);
+
+    const thumbnails = videoDetails?.thumbnail?.thumbnails.sort(
+      (a: { width: number }, b: { width: number }) => b.width - a.width
+    );
+
+    const stream = await prismaClient.stream.create({
       data: {
         userId: data.creatorId,
         url: data.url,
         extractedId,
         type: "Youtube",
+        title: videoDetails?.title ?? "Title not found",
+        bigImg:
+          thumbnails[0]?.url ??
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcats&psig=AOvVaw29Fz1wkvZPdZBF5QA2cH-u&ust=1751657624212000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDhx6W3oY4DFQAAAAAdAAAAABAE",
+        smallImg:
+          thumbnails[1]?.url ??
+          thumbnails[0]?.url ??
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Fcats&psig=AOvVaw29Fz1wkvZPdZBF5QA2cH-u&ust=1751657624212000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCNDhx6W3oY4DFQAAAAAdAAAAABAE",
       },
+    });
+
+    return NextResponse.json({
+      message: "Stream added successfully",
+      streamId: stream.id,
     });
   } catch (err) {
     return NextResponse.json(
